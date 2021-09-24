528dda8137efe9e4c93afd1c960fe7e6996d5cfd
balazskaloczi <78374955+balazskaloczi@users.noreply.github.com>
2021-09-24 08:28:42 +0200

    +1

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,10 +16,10 @@ class Header extends React.Component {
                     <motion.div initial={{x: -200}} animate={{x: 0}} exit={{x: -200}} transition={{duration: 1.5}} className="nameTag">
                         <h3>balazs.kaloczi<i className="bi bi-slash fa-4x"></i></h3>
                     </motion.div>
-                    <motion.ul initial={{x: 200}} animate={{x: 0}} exit={{x: 200}} transition={{duration: 1.5}}className={this.state.isClicked ? "nav-menu" : "nav-menu--active"}>
-                        <Link to="/"><li>Home</li></Link>
-                        <Link to="/projects"><li>Projects</li></Link>
-                        <Link to="/contact"><li>Contact</li></Link>
+                    <motion.ul initial={{x: 200}} animate={{x: 0}} exit={{x: 200}} transition={{duration: 1.5}} className={this.state.isClicked ? "nav-menu" : "nav-menu--active"}>
+                        <Link to="/"><li><h3 className="linkButton">Home</h3></li></Link>
+                        <Link to="/projects"><li><h3 className="linkButton">Projects</h3></li></Link>
+                        <Link to="/contact"><li><h3 className="linkButton">Contact</h3></li></Link>
                     </motion.ul>
                     <motion.div  initial={{x: 100}} animate={{x: 0}} exit={{x: 100}} transition={{duration: 1.5}} className="icon" onClick={this.handleClick}>
                         <motion.i  className={this.state.isClicked ? "bi bi-x fa-3x" : "bi bi-justify fa-3x"}></motion.i>
--- src/components/main.js
+++ src/components/main.js
@@ -5,12 +5,16 @@ import { motion } from 'framer-motion';
 const Main  = () => { 
 
     return (
-      <div className="mainWrapper" >
         <motion.div className="background" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 1.5}}>
-          <h3>Hello there ! </h3>
-          <p> <Link to="/contact" target="_blank" without rel="noreferrer">contact me</Link>!</p>
+          <div className="avatar">
+            <div className="avatarImage"/>
+            <p className="role">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</p>
+            <p className="introText">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic</p>
+          </div>
+          <div className="intro"> 
+            
+          </div>
         </motion.div>
-      </div>
     )}
     
 export default Main 
